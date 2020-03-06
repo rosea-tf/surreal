608c3c331ee6112ddce44ac36e5cb1eb808872fa
Alex Rose <rosea@tf.uni-freiburg.de>
2020-03-06 16:46:54 +0100

    housekeeping

--- surreal/algos/dads.py
+++ surreal/algos/dads.py
@@ -187,22 +187,6 @@ class DADS(RLAlgo):
     #            μ[i] = sum[k=0->K-1](exp(γ renv[k]) / (sum[p=0->K-1](exp(γ renv[p]))) * zk[i])
     #    # return best next plan (z).
 
-    #TODO move somewhere else - delete self.
-    @staticmethod
-    def iterate_minibatches(inputs, minisize, shuffle=False):
-        fullsize = inputs[0].shape[0]
-        assert all([fullsize == other.shape[0] for other in inputs[1:]])
-        if shuffle:
-            indices = np.arange(inputs.shape[0])
-            np.random.shuffle(indices)
-        for start_idx in range(0, fullsize, minisize):
-            end_idx = min(start_idx + minisize, fullsize)
-            if shuffle:
-                excerpt = indices[start_idx:end_idx]
-            else:
-                excerpt = slice(start_idx, end_idx)
-            yield [i[excerpt] for i in inputs]
-
 class DADSConfig(AlgoConfig):
     """
     Config object for a DADS Algo.
